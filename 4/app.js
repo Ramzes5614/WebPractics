d0f7c4e6b95248d8322b02c94634a0bc5e97e118
Iskander Adiullov <adiullov.iskander2000@gmail.com>
2021-04-14 13:24:57 +0300

    Работа вкладок

--- 4/app.js
+++ 4/app.js
@@ -1,5 +1,26 @@
 var main = function () {
 "use strict";
-console.log("hello, world!");
+$(".tabs a:nth-код для этого элемента может выглядеть вот так:child(1)").on("click", function () {
+/span></a>/span></a> делаем все вкладки неактивными
+$(".tabs span").removeClass("active");
+/span></a>/span></a> делаем активной первую вкладку
+$(".tabs a:nth-код для этого элемента может выглядеть вот так:child(1) span").addClass("active");
+/span></a>/span></a> очищаем основное содержание, чтобы переопределить его
+$("main .content").empty();
+/span></a>/span></a> возвращается false, так как мы не переходим по ссылке
+return false;
+});
+$(".tabs a:nth-код для этого элемента может выглядеть вот так:child(2)").on("click", function () {
+$(".tabs span").removeClass("active");
+4
+$(".tabs a:nth-код для этого элемента может выглядеть вот так:child(2) span").addClass("active");
+$("main .content").empty();
+return false;
+});
+$(".tabs a:nth-код для этого элемента может выглядеть вот так:child(3)").on("click", function () {
+$(".tabs span").removeClass("active");
+$(".tabs a:nth-код для этого элемента может выглядеть вот так:child(3) span").addClass("active");
+$("main .content").empty();
+return false;
+});
 };
-$(document).ready(main);
