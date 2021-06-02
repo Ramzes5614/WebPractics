0be6cd6a40cc861bdca9a1562799839dfed1aa9b
Ramzes5614 <46199138+Ramzes5614@users.noreply.github.com>
2021-06-02 12:21:08 +0300

    initial

---
+++ scr.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+git filter-branch --env-filter ' OLD_EMAIL="adiullov.iskander2000@gmail.com" CORRECT_NAME="Ramzes5614" CORRECT_EMAIL="ramil.galimov1@gmail.com" if [ "$GIT_COMMITTER_EMAIL" = "$OLD_EMAIL" ] then export GIT_COMMITTER_NAME="$CORRECT_NAME" export GIT_COMMITTER_EMAIL="$CORRECT_EMAIL" fi if [ "$GIT_AUTHOR_EMAIL" = "$OLD_EMAIL" ] then export GIT_AUTHOR_NAME="$CORRECT_NAME" export GIT_AUTHOR_EMAIL="$CORRECT_EMAIL" fi '--tag-name-filter cat -- --branches --tags
